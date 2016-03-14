bb60dacbf0b5d1119c06c939329f9a5339c9dbe0
Alexander Maksimovich <mak4alex@gmail.com>
2016-03-14 20:25:04 +0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name unicornAdminClientApp
+ * @description
+ * # unicornAdminClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('unicornAdminClientApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name unicornAdminClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the unicornAdminClientApp
+ */
+angular.module('unicornAdminClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
